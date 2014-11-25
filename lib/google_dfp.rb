b7ec107459b75dbf592e6ede597600f05b7b110d
Jason Franklin-Stokes <j.franklin-stokes@digitaleseiten.de>
2014-11-25 17:37:55 +0100

    user definable id and remove style width and height

--- lib/google_dfp.rb
+++ lib/google_dfp.rb
@@ -8,16 +8,15 @@ module GoogleDFP
   end
   
   module ViewHelper
-    def dfp_tag(name, targeting=nil)
+    def dfp_tag(name, targeting=nil, id=nil)
       tag  = GoogleDFP::Tag.get(name)
       data = tag.data
       data = data.merge(targeting: targeting) if targeting.present?
 
       content_tag :div,
         "",
-        id:    "dfp-#{name}",
+        id:    id || "dfp-#{name.gsub("_", "-")}",
         class: 'google-dfp',
-        style: tag.style,
         data:  data
     end
   end
